09a89343a7b57e49d96f5c386c949c01e5978593
Slalonxxx <89746150+Slalonxxx@users.noreply.github.com>
2021-08-29 21:25:06 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ def index():
     soup= b(content,"lxml")
     precio=soup.find("span",{"class":"Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})
     rango=soup.find("span",{"class":"Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)"})
-    return "Valor Disney:"+ str(precio.text)
+    return 'Valor Disney:'+ str(precio.text)
 
 
 if __name__ == "__main__":
