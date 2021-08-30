8cb280aa8ad90b3373d6af8438a715b014c39fdb
Slalonxxx <89746150+Slalonxxx@users.noreply.github.com>
2021-08-29 21:09:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
 
+from bs4 import BeautifulSoup as b
+url='https://finance.yahoo.com/quote/DIS?p=DIS&.tsrc=fin-srch'
+html= requests.get(url)
+content=html.content
+soup= b(content,"lxml")
+precio=soup.find("span",{"class":"Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})
+rango=soup.find("span",{"class":"Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)"})
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, world!"
+    return "Valor Disney:"+ srt(precio.txt)
 
 
 if __name__ == "__main__":
