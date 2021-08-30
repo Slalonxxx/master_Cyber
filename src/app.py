52cd23e37f9c63910a0663032a10005e2f03ff29
Slalonxxx <89746150+Slalonxxx@users.noreply.github.com>
2021-08-29 21:30:44 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+
 import requests
 from bs4 import BeautifulSoup as b
 
@@ -6,13 +7,21 @@ from bs4 import BeautifulSoup as b
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    url='https://finance.yahoo.com/quote/DIS?p=DIS&.tsrc=fin-srch'
-    html= requests.get(url)
-    content=html.content
-    soup= b(content,"lxml")
-    precio=soup.find("span",{"class":"Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})
-    rango=soup.find("span",{"class":"Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)"})
-    return str(precio.text)
-if __name__ == "__main__":
-    app.run()
+def home():
+	url='https://finance.yahoo.com/quote/DIS?p=DIS&.tsrc=fin-srch'
+	html= requests.get(url)
+	content=html.content
+	soup= b(content,"lxml")
+	precio=soup.find("span",{"class":"Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})
+	rango=soup.find("span",{"class":"Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)"})
+	data={
+		'curso' : 'CyberFisicos',
+		'nombre':'Edison',
+		'precio': str(precio.text),
+		'rango': str(rango.text) 
+	}
+	
+	return render_template('reto1.html', data=data)
+   
+if __name__ == '__main__':
+	app.run()
