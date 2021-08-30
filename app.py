d9fb201c1e8869ddbf0c6fc133af145edf1475f7
Slalonxxx <89746150+Slalonxxx@users.noreply.github.com>
2021-08-29 21:08:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,11 @@
-from flask import Flask, render_template
-
-import requests
-from bs4 import BeautifulSoup as b
-url='https://finance.yahoo.com/quote/DIS?p=DIS&.tsrc=fin-srch'
-html= requests.get(url)
-content=html.content
-soup= b(content,"lxml")
-precio=soup.find("span",{"class":"Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})
-rango=soup.find("span",{"class":"Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)"})
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-	data={
-		'curso' : 'CyberFisicos',
-		'nombre':'Edison',
-		'precio': str(precio.text),
-		'rango': str(rango.text) 
-	}
-	
-	return 'Precio Disney' + str(precio.text)
-   
-if __name__ == '__main__':
-	app.run()
+def index():
+    return "Hello, world!"
+
+
+if __name__ == "__main__":
+    app.run()
